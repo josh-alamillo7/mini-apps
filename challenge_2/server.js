70736bd0db7755925b9823015110108224fbd81b
Josh Alamillo <josh.alamillo@gmail.com>
2018-02-13 12:54:03 -0800

    finally got my server working

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,11 @@
+const http = require('http');
+const express = require('express');
 
+//****************** THE SERVER **********************
 
+http.createServer(function (req, res) {
+	res.write('Hello world!');
+	res.end();
+}).listen(8000, '127.0.0.1', () => {
+	console.log("listening on port 8000");
+});
