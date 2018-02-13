46d269acaa4f9148ef7d8ef700c8f47e52f1b21c
Josh Alamillo <josh.alamillo@gmail.com>
2018-02-13 13:10:37 -0800

    added some pseudocode describing what I want the request handler to do

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,11 +1,31 @@
 const http = require('http');
 const express = require('express');
 
+//initialize a global storage.
+var objectsReceived = {};
+
+
+//***************** REQUEST HANDLER *****************
+
+//logic for the request handler should go here.
+
+const requestHandler = function(req, res) {
+	console.log("u sent it")
+}
+
+
+//***************CASE HANDLER FUNCTIONS***************
+
+//meant to act as a controller/model.
+//on a get, this should pull all the data out of our storage as CSV's.
+//here, the input will be JSON data. (JavaScript Object)
+//on a post, this should add the received object to our storage.
+
 //****************** THE SERVER **********************
 
-http.createServer(function (req, res) {
-	res.write('Hello world!');
-	res.end();
-}).listen(8000, '127.0.0.1', () => {
+http.createServer(requestHandler).listen(8000, '127.0.0.1', () => {
 	console.log("listening on port 8000");
 });
+
+
+
