45e07d84ed1dd987747511be5932728bce60d485
Josh Alamillo <josh.alamillo@gmail.com>
2018-02-14 14:50:05 -0800

    trying to get all the dependencies working

---
+++ challenge_3/client/app.jsx
@@ -0,0 +1,35 @@
+//*****************import React********************
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+//****************Components***********************
+
+class Board extends React.Component {
+
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		//test with Hello World for now
+		//return <div>Hello World</div>;
+	}
+
+}
+console.log("hello")
+
+ReactDOM.render(<Board />, document.getElementById('app'));
+
+
+//******************Views**************************
+
+
+
+
+
+
+
+
+
+
+//****************Tests****************************
